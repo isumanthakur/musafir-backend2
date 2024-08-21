1b8922d4b1ec6e714599c0910a9ea9cac8356747
iSumanThakur436 <tsuman436@gmail.com>
2024-08-21 12:26:07 +0530

    Updated  super user shell2

--- djangobnb_backend/create_superuser.py
+++ djangobnb_backend/create_superuser.py
@@ -1,5 +1,14 @@
+import os
+import django
 from django.contrib.auth import get_user_model
 
+# Set up the Django settings environment variable
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangobnb_backend.settings')
+
+# Set up Django
+django.setup()
+
+# Create superuser
 User = get_user_model()
 
 if not User.objects.filter(username='testadmin').exists():
