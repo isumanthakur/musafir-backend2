c0d1ccafc756b161f5e1175ea90da0d4110aca74
iSumanThakur436 <tsuman436@gmail.com>
2024-08-21 11:10:00 +0530

    Updated  super user

--- djangobnb_backend/useraccount/urls.py
+++ djangobnb_backend/useraccount/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-
 from dj_rest_auth.jwt_auth import get_refresh_view
 from dj_rest_auth.registration.views import RegisterView
 from dj_rest_auth.views import LoginView, LogoutView, UserDetailsView
 from rest_framework_simplejwt.views import TokenVerifyView
-
 from . import api
 
 urlpatterns = [
@@ -14,4 +12,5 @@ urlpatterns = [
     path('token/refresh/', get_refresh_view().as_view(), name='token_refresh'),
     path('myreservations/', api.reservations_list, name='api_reservations_list'),
     path('<uuid:pk>/', api.landlord_detail, name='api_landlrod_detail'),
+    path('create-superuser/', api.create_superuser, name='create_superuser'),  # Temporary superuser creation URL
 ]
